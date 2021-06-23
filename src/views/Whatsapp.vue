a84e411957df257a7379b24d58b3509c711274c3
Ismael Souza <souz4ismael@gmail.com>
2021-06-23 09:25:37 -0300

    Created router for wpp

---
+++ src/views/Whatsapp.vue
@@ -0,0 +1,16 @@
+<template>
+    <Home />
+</template>
+
+<script>
+import Home from './Home.vue'
+export default {
+  components: {
+    Home,
+  },
+  created() {
+    const url = `https://api.whatsapp.com/send?phone=${5571999992891}&text=Olá, gostaria de ver o cardapio!`;
+    window.location = url;
+  },
+};
+</script>
