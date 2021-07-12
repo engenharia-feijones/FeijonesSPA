34a53efe223bed6df64f49d2c590dfc0b4a2d05c
Ismael Souza <souz4ismael@gmail.com>
2021-07-12 18:44:40 -0300

    added axios and create a ip tracker

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import axios from "axios";
 import IndexHome from "../views/Home/Index.vue";
 import IndexAbout from "../views/About/Index.vue";
 import IndexService from "../views/Service/Index.vue";
@@ -12,6 +13,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.name === "Home" && !from.name) {
+    const { data } = await axios.get("https://api.db-ip.com/v2/free/self");
+    const ipAddress = { ip: data.ipAddress, date: "" };
+    axios
+      .post("https://ssacapsapi.azurewebsites.net/api/AcessIP", ipAddress)
+      .then(console.log);
+  }
+
+  next();
 });
 
 export default router;
